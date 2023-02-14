9f209c21a4bce50d03fb3ef4091375f47891eae1
Ben Abdallah Houda <ahhouda@yahoo.fr>
2023-02-14 18:46:31 +0100

    fonction filtre

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -18,26 +18,26 @@ getData("http://localhost:5678/api/works");
 //ajouter les bouttons de filtre
 const sectionFilter = document.querySelector(".filter");
 sectionFilter.innerHTML =
-    '<button id="button-all-works" class ="filter-button" type= "button"> Tous</button>' +
-    '<button id="button-object" class ="filter-button"> Objets</button>' +
-    '<button id="button-apartment" class ="filter-button"> Appartements</button>' +
-    '<button id="button-hotel" class ="filter-button"> Hôtels & restaurants</button>';
+    '<button id="button-all-works" class="filter-button" type= "button"> Tous</button>' +
+    '<button id="button-object" class="filter-button"> Objets</button>' +
+    '<button id="button-apartment" class="filter-button"> Appartements</button>' +
+    '<button id="button-hotel" class="filter-button"> Hôtels & restaurants</button>';
 
-const buttonAllWorks = document.getElementById("button-all-works");
-buttonAllWorks.addEventListener("click", function () {
+const buttonAllCategory = document.getElementById("button-all-works");
+buttonAllCategory.addEventListener("click", function () {
     document.querySelector(".gallery").innerHTML = "";
     getData("http://localhost:5678/api/works");
 });
-const buttonFiltreObject = document.getElementById("button-object");
-buttonFiltreObject.addEventListener("click", function () {
+const buttonCategory1 = document.getElementById("button-object");
+buttonCategory1.addEventListener("click", function () {
     filtreProjects(1);
 });
-const buttonFiltreApartement = document.getElementById("button-apartment");
-buttonFiltreApartement.addEventListener("click", function () {
+const buttonCategory2 = document.getElementById("button-apartment");
+buttonCategory2.addEventListener("click", function () {
     filtreProjects(2);
 });
-const buttonFiltreHotel = document.getElementById("button-hotel");
-buttonFiltreHotel.addEventListener("click", function () {
+const buttonCategory3 = document.getElementById("button-hotel");
+buttonCategory3.addEventListener("click", function () {
     filtreProjects(3);
 });
 //fonction d'affichage des projets
@@ -50,24 +50,27 @@ const afficherProject=(works)=> {
         titleElement.innerText = article.title;
         const imageElement = document.createElement("img");
         imageElement.src = article.imageUrl;
-        worksElement.setAttribute("id-cat", article.categoryId);
+        //imageElement.classList.add("active");
+        const categorId = document.createElement("p")
+        categorId.innerText= article.categoryId;
+       // worksElement.setAttribute("data-cat", article.categoryId);
         sectionGallery.appendChild(worksElement);
         worksElement.appendChild(imageElement);
         worksElement.appendChild(titleElement);
+        
     }
 }
 
-//fonction de filtre des projets
+
+// //fonction de filtre des projets
 const filtreProjects=(id)=> {
     document.querySelector(".gallery").innerHTML = "";
-    const project =getData("http://localhost:5678/api/works");
-    for (let elem in project) {
-        if ( '${article.categoryId}' != id) {
-           delete project[elem];
+    const project = getData("http://localhost:5678/api/works")
+    for (let elem in project){
+        if(categorId != id){
+           delete project[elem ]
         }
     }
-    //2eme essaie
-    //const resultatFiltre = project.filter(project =>project.article.categoryId=id);
 
 }
 
